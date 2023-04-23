9148cc3076ef71d6c5b8e7c68381db4dbf280fd5
AndreFeMarques <1200605@isep.ipp.pt>
2023-04-23 18:00:50 +0100

    sobre nos e animacoes

--- src/app/home-landing2/home-landing2.component.ts
+++ src/app/home-landing2/home-landing2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-landing2',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-landing2.component.css']
 })
 export class HomeLanding2Component {
+
+  @ViewChild('image', { static: false }) image!: ElementRef | undefined; // Reference to the image element in the template
+
+  constructor(private renderer: Renderer2) {}
+
+  onMouseEnter() {
+    console.log("entered the code");
+    if (this.image) {
+      console.log(this.image);
+      // Animate the image when mouse enters
+      this.renderer.setStyle(this.image.nativeElement, 'transform', 'rotate(360deg)');
+    }
+  }
+
+  onMouseLeave() {
+    if (this.image) {
+      // Animate the image when mouse leaves
+      this.renderer.setStyle(this.image.nativeElement, 'transform', 'rotate(0deg)');
+    }
+  }
 
 }
